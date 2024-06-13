e7f231a42b8a20a33f00c3278ed954751c5d17cc
love-ara <aramideotenaike@gmail.com>
2024-06-13 18:47:17 +0100

    share feature added

--- src/component/preview/index.jsx
+++ src/component/preview/index.jsx
@@ -41,11 +41,14 @@ const Preview = () => {
                                      borderRadius: "15px",
                                      border: "white solid ",
                                  }}/>
-                            <div style={{display: "none"}}>
-                                <p>SHARE</p>
-                                <img src={facebook} alt="facebook"/>
-                                <img src={twitter} alt="twitter"/>
-                                <img src={pinterest} alt="pinterest"/>
+                            <div className={style.share}>
+                                <div className={style.shareDiv}>
+                                    <p>SHARE</p>
+                                    <img src={facebook} alt="facebook"/>
+                                    <img src={twitter} alt="twitter"/>
+                                    <img src={pinterest} alt="pinterest"/>
+                                </div>
+
                             </div>
                         </div>
 
